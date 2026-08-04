db4ff9c0cc1b0dd2669fedcd02a57103c094ef0c
llancor <llancor@gmail.com>
2026-08-04 19:23:06 -0400

    Delete GuardiaPro-beta directory

--- GuardiaPro-beta/backend/src/types.d.ts
+++
@@ -1,2 +0,0 @@
-declare namespace Express { interface Request { user?: { id:string; role:string; permisos:Record<string,boolean> } } }
-
--- GuardiaPro-beta/frontend/src/main.tsx
+++
@@ -1,2 +0,0 @@
-import React from 'react';import{createRoot}from'react-dom/client';import{BrowserRouter}from'react-router-dom';import'./index.css';import App from'./App';import{AuthProvider}from'./state';import{BrandingProvider}from'./branding';
-createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter><BrandingProvider><AuthProvider><App/></AuthProvider></BrandingProvider></BrowserRouter></React.StrictMode>);
