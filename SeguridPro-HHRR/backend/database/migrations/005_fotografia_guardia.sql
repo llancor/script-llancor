2bae6e3ed4c820a2ca1b7f2498e117104fd07070
llancor <llancor@gmail.com>
2026-08-13 11:30:33 -0400

    Delete SeguridPro-HHRR directory

--- SeguridPro-HHRR/backend/database/migrations/005_fotografia_guardia.sql
+++
@@ -1,3 +0,0 @@
-USE seguridad_rrhh;
-SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE guardias MODIFY COLUMN foto_url MEDIUMTEXT NULL;
--- SeguridPro-HHRR/backend/dist/src/shift-utils.js
+++
@@ -1,4 +0,0 @@
-export const timeMinutes = (value) => { if (!/^([01]\d|2[0-3]):[0-5]\d$/.test(value))
-    throw new Error('Horario inválido'); const [h, m] = value.split(':').map(Number); return h * 60 + m; };
-export const shiftHours = (start, end) => { const a = timeMinutes(start), b = timeMinutes(end); return ((b > a ? b : b + 1440) - a) / 60; };
-export const overlaps = (aStart, aEnd, bStart, bEnd) => aStart < bEnd && bStart < aEnd;
